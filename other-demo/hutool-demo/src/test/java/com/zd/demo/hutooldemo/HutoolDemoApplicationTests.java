0e8aacd7388cd658a54d840b8950c811f08e7d9d
zhangdi <zhangdi03@inspur.com>
2020-01-20 14:43:49 +0800

    init commit

---
+++ other-demo/hutool-demo/src/test/java/com/zd/demo/hutooldemo/HutoolDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zd.demo.hutooldemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HutoolDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
